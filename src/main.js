322702c7ded3c13b590e9c4602a815348797ef43
Ronaldo Rodríguez Bermúdez <r2.bermudez@gmail.com>
2023-11-05 14:45:31 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ import './assets/css/skeleton.css';
 
 const app = createApp(App);
   
-app.config.globalProperties.url = 'https://backend-.netlify.app';
+app.config.globalProperties.url = 'https://backend-mongo.netlify.app';
 
 app.use(router).mount("#app");
