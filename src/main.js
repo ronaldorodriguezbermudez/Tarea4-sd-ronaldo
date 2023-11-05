96ecb7c7a96633ea6251d96d70bccf0c517bc1a4
Ronaldo Rodríguez Bermúdez <r2.bermudez@gmail.com>
2023-11-05 14:59:43 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ import './assets/css/skeleton.css';
 
 const app = createApp(App);
   
-app.config.globalProperties.url = 'https://backend-mongo.netlify.app';
+app.config.globalProperties.url = 'https://tarea4-sd-ronaldo.netlify.app';
 
 app.use(router).mount("#app");
