37014f23c7173b0b43d3b4237331daee6d58741a
Ronaldo Rodríguez Bermúdez <r2.bermudez@gmail.com>
2023-11-05 09:32:36 -0600

    Update login.vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -2,34 +2,53 @@
     <form @submit.prevent="submitForm">
       <h2>Login</h2>
       <div class="input-field">
-        <input type="text" v-model="username" placeholder="Enter Username">
+        <input type="text" v-model="formData.username" placeholder="Enter Username">
       </div>
       <div class="input-field">
-        <input type="password" v-model="password" placeholder="Enter Password">
+        <input type="password" v-model="formData.password" placeholder="Enter Password">
       </div>
       <input type="submit" value="LogIn">
     </form>
   </template>
   
   <script>
-  export default {
-    data() {
-      return {
-        username: '',
-        password: ''
-      }
-    },
+export default {
+  data() {
+    return {
+      formData: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+    
     methods: {
-      submitForm() {
-        // Handle form submission here
-        // You might want to call an authentication service here
-        console.log(`Username: ${this.username}, Password: ${this.password}`);
-      }
-    }
-  }
-  </script>
+        async Login(event){
+            event.preventDefault();
+            try {
+                const response = await fetch('http://tarea4-sd-ronaldo.netlify.app/.netlify/funtions/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(this.formData),
+                });
+
+                if (response.status === 200) {
+                    const data = await response.json();
+                    // Guarda el token en una cookie en lugar de localStorage
+                    document.cookie = `token=${data.token};path=/`;
+                    this.$router.push("/home");
+                } else {
+                    console.error('Error de autenticación');
+                }
+                } catch (error) {
+                    console.error(error);
+                }
+    },
+  },
+};  
+</script>
   
-  <style scoped>
-  /* Add your CSS styles here */
-  </style>
+
 
